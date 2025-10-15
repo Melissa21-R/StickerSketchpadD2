05c3959fba0569749c0a9c2f2af76da8124d9766
Melissa21-R <108831978+Melissa21-R@users.noreply.github.com>
2025-10-15 00:10:04 +0000

    created my tile and my canvas as well as a styled body to center everything in the center of the screen (step 1 complete)

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,15 @@
-import exampleIconUrl from "./noun-paperclip-7598668-00449F.png";
 import "./style.css";
 
-document.body.innerHTML = `
-  <p>Example image asset: <img src="${exampleIconUrl}" class="icon" /></p>
-`;
+//make the title for the top of the screen
+const title = document.createElement("h1");
+title.textContent = "Sketch Pad";
+
+//create the canvas
+const canvas = document.createElement("canvas");
+canvas.width = 256;
+canvas.height = 256;
+canvas.id = "skCanvas"; //making it easy to style
+
+//Apending children station
+document.body.appendChild(title);
+document.body.appendChild(canvas);
